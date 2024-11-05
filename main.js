27d72497b977c2bfe8b2e6def916dc6041c55684
Andrea Verlicchi <verlok@users.noreply.github.com>
2024-11-05 17:30:12 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,1 @@
+const jsCtrlId = 'hVZhab'; for (const controller of document.querySelectorAll(`[jscontroller="${jsCtrlId}"]`)) if (controller.style.display === '') controller.style.display = 'none'; else controller.style.display = '';
